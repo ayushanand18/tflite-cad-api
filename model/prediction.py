8ecfb296f2b728391e0d3119e010a10e14544141
Ayush Anand <aa.21u10425@btech.nitdgp.ac.in>
2022-12-25 06:00:18 +0000

    feat: setting up the prediction pipeline

--- model/prediction.py
+++ model/prediction.py
@@ -1,2 +1,29 @@
 def process(query):
-    return query
+    # write pre-processing code here
+    return predict(query)
+
+def predict(things):
+    interpreter = tf.lite.Interpreter(model_path = ".tflite")
+    interpreter.allocate_tensors()
+    
+    # get the input and output details
+    input_details = interpreter.get_input_details()
+    output_details = interpreter.get_output_details()
+    
+    # set the input data
+    input_shape = input_details[0]['shape']
+    input_data = data
+    interpreter.set_tensor(input_details[0]['index'], input_data)
+
+    # predict
+    interpreter.invoke()
+
+    # print the output
+    output_data = interpreter.get_tensor(output_details[0]['index'])
+    print(result(output_data))
+
+def result(data):
+    if data[0]>0.5:
+        return (True, data[0])
+    else:
+        return(False, 1-data[0])
